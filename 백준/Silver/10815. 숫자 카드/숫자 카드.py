b9494e8fc5e771f0e717366151251a1f9587773e
gk3126 <92345429+gk3126@users.noreply.github.com>
2023-01-18 19:19:43 +0700

    [Silver V] Title: 숫자 카드, Time: 4240 ms, Memory: 111416 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,20 @@
+n = int(input())
+nums = list(map(int,input().split()))
+m = int(input())
+mnums = list(map(int,input().split()))
+
+nums.sort()
+for mnum in mnums:
+    left = 0
+    right = n - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == mnum:
+            print("1",end = " ")
+            break
+        elif nums[mid] < mnum:
+            left = mid + 1
+        elif nums[mid] > mnum:
+            right = mid - 1
+    if left > right:
+        print("0",end = " ")
