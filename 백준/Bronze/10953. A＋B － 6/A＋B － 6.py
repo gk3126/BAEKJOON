5f0ea3db96a9bbf2b9e0b37117d96cf6773e53f1
gk3126 <92345429+gk3126@users.noreply.github.com>
2023-02-01 18:15:32 +0700

    [Bronze II] Title: A+B - 6, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.py
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.py
@@ -1,5 +1,6 @@
 t = int(input())
 
 for i in range(t):
-    a,b = map(int,input().split(','))
+    a,b = map(int,input().split(','))       # split으로 A,B로 구분을 지어서 할 수가 있음. 
+                                            # 그냥 쓴다면 기본적으로 공백으로 구분이 됨.
     print(a + b)
